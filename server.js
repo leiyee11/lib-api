472dea3e316d1c332363a6448a748598fd44a907
leiyee11 <leiyeeshun11@gmail.com>
2021-12-30 12:52:53 +0630

    add fb

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const cors = require('cors');
 const config = require('./DB');
 const userRoute = require('./routes/UserRoute');
 
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook');
+
 const PORT = process.env.PORT || 3000;
 
 mongoose.set('useNewUrlParser', true);
@@ -34,3 +37,47 @@ app.use('/api/users', userRoute);
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
+
+
+
+passport.use(new FacebookStrategy({
+    clientID: process.env['597912377939921'],
+    clientSecret: process.env['afffcdc940c8641dff25f7691273487a'],
+    callbackURL: 'http://localhost:4200/oauth2/redirect/facebook'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
+      'https://www.facebook.com',
+      profile.id
+    ], function(err, cred) {
+      if (err) { return cb(err); }
+      if (!cred) {
+        db.run('INSERT INTO users (name) VALUES (?)', [
+          profile.displayName
+        ], function(err) {
+          if (err) { return cb(err); }
+
+          var id = this.lastID;
+          db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
+            id,
+            'https://www.facebook.com',
+            profile.id
+          ], function(err) {
+            if (err) { return cb(err); }
+            var user = {
+              id: id.toString(),
+              name: profile.displayName
+            };
+            return cb(null, user);
+          });
+        });
+      } else {
+        db.get('SELECT * FROM users WHERE id = ?', [ cred.user_id ], function(err, user) {
+          if (err) { return cb(err); }
+          if (!user) { return cb(null, false); }
+          return cb(null, user);
+        });
+      }
+    }
+  )}
+));
